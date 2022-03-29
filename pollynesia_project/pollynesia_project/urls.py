e6c25939476933a2fec8bfae6f0c77aa06be2227
Dominik Gulacsy <40367890+dgulacsy@users.noreply.github.com>
2022-03-29 18:11:34 +0200

    add basic profile page

--- pollynesia_project/pollynesia_project/urls.py
+++ pollynesia_project/pollynesia_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from re import template
+from xml.dom.minidom import Document
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as users_views
 
@@ -28,3 +31,6 @@ urlpatterns = [
     path('', include('pollynesia_site.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
