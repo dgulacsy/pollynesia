a0f25f489d2abd92f98f45a02491b956a0a225fc
Dominik Gulacsy <40367890+dgulacsy@users.noreply.github.com>
2022-03-29 21:39:17 +0200

    improve profile page

--- pollynesia_project/users/models.py
+++ pollynesia_project/users/models.py
@@ -1,6 +1,14 @@
 from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,4 +16,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self):
+        super().save()
+        with Image.open(self.image.path) as img:
+            if img.height > 300 or img.width > 300:
+                output_size = (300,300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
--- pollynesia_project/users/views.py
+++ pollynesia_project/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserSignupForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserSignupForm, UserUpdateForm, ProfileUpdateForm
+
 
 def signup(request):
     if request.method == 'POST':
@@ -9,12 +10,31 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created, {username}! You can log in now!')
+            messages.success(
+                request, f'Your account has been created, {username}! You can log in now!')
             return redirect('polls:index')
     else:
         form = UserSignupForm()
     return render(request, 'users/signup.html', {'form': form})
 
+
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'You have update your profile data.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context=context)
